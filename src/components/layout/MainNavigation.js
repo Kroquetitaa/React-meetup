f371f9945d150491981da063544ba1468edd0f4b
Kroquetita <kroquetita@gmail.com>
2024-01-12 20:09:54 +0100

    part-1 assigned scroll-up and down for header

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
 
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation({ setPage }) {
+
+  const [prevY, setPrevY ] = useState(window.scrollY)
+  const [ isVisible, setIsVisible ] = useState(true)
+
+  const handleScroll = () => {
+    const currentY = window.scrollY
+    setIsVisible(prevY > currentY || currentY === 0)
+    setPrevY(currentY)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [prevY])
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header className={`${classes.header} ${isVisible ? '' : classes.off}`} data-test="navigation-header">
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
